0d658924ab946053f5f532398be8e376fe1fe6da
nemdull <dlanx@outlook.jp>
2022-03-08 00:52:32 +0900

    CreatePostsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations:"registrations"}
-  
+
   root "top#index"
+
+  resources :posts, only: %i(new create) do
+    resources :arts, only: %i(create)
+  end
 
   resources :users, only:[:show,:index]
   get "/swipe", to: "posts#swipe"
