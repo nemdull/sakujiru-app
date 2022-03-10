051903ab26627f5fa6ae62e88ca2ac3eca94cc34
dlanx0607 <dlanx@outlook.jp>
2022-03-10 22:26:25 +0900

    fix redistrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     swipe_path(resource)
   end
+
+  def after_sign_up_path_for(resource)
+    swipe_path
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_090751) do
+ActiveRecord::Schema.define(version: 2022_03_10_014354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,15 +41,6 @@ ActiveRecord::Schema.define(version: 2022_03_10_090751) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "reactions", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_reactions_on_post_id"
-    t.index ["user_id"], name: "index_reactions_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -69,6 +60,4 @@ ActiveRecord::Schema.define(version: 2022_03_10_090751) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
-  add_foreign_key "reactions", "posts"
-  add_foreign_key "reactions", "users"
 end
