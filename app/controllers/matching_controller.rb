d93bd44cdfd5a927c3b3d3a1197c4749929d0177
nemdull <dlanx@outlook.jp>
2022-03-11 16:46:16 +0900

    fix matching_controller

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -2,7 +2,9 @@ class MatchingController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # 自分がいいねした投稿のidを抜く
     reaction_post_ids = Reaction.where(user_id: current_user.id).pluck(:post_id)
+    # 投稿のうちいいねしたものを抜く
     @posts = Post.where(id: reaction_post_ids)
   end
 end
