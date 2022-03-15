7cd95a7649383b2928e94e7fa6536a9711b512a8
nemdull <dlanx@outlook.jp>
2022-03-15 16:58:27 +0900

    fix user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,9 @@ class UsersController < ApplicationController
   end
 
   def show
+    # URLのuserを取得
     @user = User.find(params[:id])
+    # URLのuserのポストを取得
+    @posts = Post.where(user_id: @user.id)
   end
 end
