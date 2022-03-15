dd06f166f014d654c5dfedf19c7c0387a0b520a2
nemdull <dlanx@outlook.jp>
2022-03-15 16:30:13 +0900

    fix avatar on navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!,except: [:index]
   def new
     @post = Post.new
     @post.arts.build
