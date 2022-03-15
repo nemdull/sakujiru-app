5e0b825b0fc64f99b5142aba0edc4ccf0c902f2c
dlanx0607 <dlanx@outlook.jp>
2022-03-15 16:30:13 +0900

    fix avatar on navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  before_action :set_current_user
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :profile_image])
+  end
+
+  def set_current_user
+    @set_current_user = User.find(current_user.id)
   end
 end
