6d76af49fae6248b71f34c0b9fa01c061686ee9d
nemdull <dlanx@outlook.jp>
2022-04-04 22:52:41 +0900

    fix carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,6 +6,6 @@ if Rails.env.production?
       aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
       region: "ap-northeast-1"
     }
-    config.fog_directory = "sakujiru"
+    config.fog_directory = sakujiru
   end
 end
